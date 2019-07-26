b67ca937c46dd257b626d5175bc662c2ce8a4e4f
梁柱 <ymblender@gmail.com>
2019-07-26 18:39:20 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,13 @@
+# css-in JS
+
+Only 0.5k, Ease use
+
+```js
+import cssin from 'cssin';
+
+cssin(`
+  body {
+    margin: 0px
+  }
+`);
+```
---
+++ lib/index.ts
@@ -0,0 +1,28 @@
+
+const getRandomString = (prefix = '', length = 12) => {
+  //设置随机数范围
+  const charts = 'abcdefghijklmnopqrstuvwxyz_0123456789';
+  const maxPos = charts.length;
+  let result = prefix;
+  for (let i = 0; i < length - 1; i++) {
+    result += charts.charAt(Math.floor(Math.random() * maxPos));
+  }
+
+  return result;
+};
+
+export const cssInJs = (value: string, replace='##') => {
+  let name = '';
+  if (value.indexOf(replace) === 0) {
+    name = getRandomString('r');
+    value = `${value.replace(replace, `.${name}`)}`;
+  }
+
+  const style = document.createElement('style');
+  style.innerHTML = value;
+  document.head.appendChild(style);
+
+  return name;
+};
+
+export default cssInJs;
