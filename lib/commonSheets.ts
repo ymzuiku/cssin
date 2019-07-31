e0b741467b80a6f1d4a451a54b03751e1d90626e
梁柱 <liangzhu@77ircloud.com>
2019-07-31 23:05:36 +0800

    feat: update-version

---
+++ lib/commonSheets.ts
@@ -0,0 +1,78 @@
+import { addSheets } from "./index";
+
+const flexCode = {
+  start: "flex-start",
+  end: "flex-end",
+  between: "space-between",
+  around: "space-around"
+};
+
+export const commonSheets = {
+  dis: (v: any) => `{ display: ${v}; }`,
+  items: (v: any) => `{ align-items: ${(flexCode as any)[v] || v};}`,
+  justify: (v: any) => `{ justify-content: ${(flexCode as any)[v] || v}; }`,
+  self: (v: any) => `{ align-self: ${(flexCode as any)[v] || v} };`,
+  content: (v: any) => `{ align-content: ${(flexCode as any)[v] || v}; }`,
+  z: (v: any) => `{ z-index: ${v}; }`,
+  // positions
+  p: (v: any) => `{ padding: ${v}; }`,
+  px: (v: any) => `{ padding-left: ${v}; padding-right: ${v};}`,
+  py: (v: any) => `{ padding-top: ${v}; padding-bottom: ${v}; }`,
+  pl: (v: any) => `{ padding-left: ${v}; }`,
+  pt: (v: any) => `{ padding-top: ${v}; }`,
+  pr: (v: any) => `{ padding-right: ${v}; }`,
+  pb: (v: any) => `{ padding-bottom: ${v}; }`,
+  m: (v: any) => `{ margin: ${v}; }`,
+  mx: (v: any) => `{ margin-left: ${v}; margin-right: ${v}; }`,
+  my: (v: any) => `{ margin-top: ${v}; margin-bottom: ${v}; }`,
+  ml: (v: any) => `{ margin-left: ${v}; }`,
+  mt: (v: any) => `{ margin-top: ${v} };`,
+  mr: (v: any) => `{ margin-right: ${v}; }`,
+  mb: (v: any) => `{ margin-bottom: ${v}; }`,
+  w: (v: any) => `{ width: ${v} }`,
+  minw: (v: any) => `{ min-width: ${v}; }`,
+  maxw: (v: any) => `{ max-width: ${v}; }`,
+  mmw: (v: any) => `{ width: ${v}; max-width: ${v}; min-width: ${v}; }`,
+  h: (v: any) => `{ height: ${v}; }`,
+  minh: (v: any) => `{ min-height: ${v}; }`,
+  maxh: (v: any) => `{ maxHeight: ${v}; }`,
+  mmh: (v: any) => `{ height: ${v}; min-height: ${v}; max-width: ${v}; }`,
+  b: (v: any) => `{ border: ${v}; }`,
+  bt: (v: any) => `{ border-top: ${v}; }`,
+  br: (v: any) => `{ border-right: ${v}; }`,
+  bb: (v: any) => `{ border-bottom: ${v}; }`,
+  bl: (v: any) => `{ border-left: ${v}; }`,
+  "b-color": (v: any) => `{ border-color: ${v}; }`,
+  "b-style": (v: any) => `{ border-style: ${v}; }`,
+  radius: (v: any) => `{ border-radius: ${v}; }`,
+  "radius-t-l": (v: any) => `{ border-top-left-radius: ${v}; }`,
+  "radius-t-r": (v: any) => `{ border-top-right-radius: ${v}; }`,
+  "radius-b-l": (v: any) => `{ border-bottom-left-radius: ${v}; }`,
+  "radius-b-r": (v: any) => `{ border-bottom-right-radius: ${v}; }`,
+  font: (v: any) => `{ font-size: ${v}; }`,
+  // theme
+  bg: (v: any) => `{ background: ${v}; }`,
+  bgc: (v: any) => `{ background-color: ${v}; }`,
+  // anime
+  "ease-in": (v: any) => `{ transition: all ${v} ease-in; }`,
+  "ease-in-out": (v: any) => `{ transition: all ${v} ease-in-out; }`,
+  scale: (v: any) => `{ transform: scale(${v}, ${v})}; }`,
+  center:
+    "display:flex; flex-direction:column; justify-content:center; align-items:center;",
+  full: "width:100%; height:100%;",
+  row: "display:flex; flex-direction:row;",
+  "row-reverse": "display:flex; flex-direction:row-reverse;",
+  col: "display:flex; flex-direction:column;",
+  "col-reverse": "display:flex; flex-direction:column-reverse;",
+  fixed: "position:fixed;",
+  static: "position:static;",
+  absolute: "position:absolute;",
+  relative: "position:relative;",
+  sticky: "position:sticky;",
+  left: "left:0px;",
+  top: "top:0px;",
+  right: "right:0px;",
+  bottom: "bottom:0px;"
+};
+
+addSheets(commonSheets);
---
+++ lib/index.ts
@@ -0,0 +1,159 @@
+import * as device from "./device";
+
+export { device };
+
+const sheets = new Map();
+const coverCache = new Set();
+
+export const coverAttribute = (attribute = "inlist") => {
+  if (coverCache.has(attribute)) {
+    return;
+  }
+  coverCache.add(attribute);
+  // tslint:disable
+  const docCreate = document.createElement;
+  document.createElement = function(name: any, option: any) {
+    const ele = docCreate.call(document, name, option);
+
+    const setAttribute = ele.setAttribute;
+    ele.setAttribute = (name: any, value: any) => {
+      if (name === attribute) {
+        if ((ele as any).__temp_class__) {
+          setAttribute.call(
+            ele,
+            "class",
+            `${cssin(value)} ${(ele as any).__temp_class__}`
+          );
+        } else {
+          setAttribute.call(ele, "class", cssin(value));
+        }
+      } else if (name === "class") {
+        (ele as any).__temp_class__ = value;
+      } else {
+        setAttribute.call(ele, name, value);
+      }
+    };
+    return ele;
+  };
+  // tslint:enable
+};
+
+export const addSheets = (objs: { [key: string]: any }) => {
+  Object.keys(objs).forEach(key => {
+    sheets.set(key, objs[key]);
+  });
+
+  return sheets;
+};
+
+addSheets({
+  "@sm": (v: string) => `@media (min-width: 640px) {${v}}`,
+  "@md": (v: string) => `@media (min-width: 768px) {${v}}`,
+  "@lg": (v: string) => `@media (min-width: 1024px) {${v}}`,
+  "@xl": (v: string) => `@media (min-width: 1280px) {${v}}`,
+  "@ios": (v: string) =>
+    `@media (min-width: ${device.isIos ? "0px" : "9999px"}) {${v}}`,
+  "@android": (v: string) =>
+    `@media (min-width: ${device.isAndroid ? "0px" : "9999px"}) {${v}}`,
+  "@native": (v: string) =>
+    `@media (min-width: ${device.isNative ? "0px" : "9999px"}) {${v}}`,
+  "@pc": (v: string) =>
+    `@media (min-width: ${device.isPc ? "0px" : "9999px"}) {${v}}`
+});
+
+const appendCssCache = new Set();
+
+const appendCss = (css: string) => {
+  if (appendCssCache.has(css)) {
+    return;
+  }
+  appendCssCache.add(css);
+  const ele = document.createElement("style");
+  ele.innerHTML = css;
+  // tslint:disable-next-line
+  ele.type = "text/css";
+  document.head.appendChild(ele);
+};
+
+const cssinCache = new Map();
+
+export const cssin = (inlist: any) => {
+  const param = typeof inlist === "string" ? inlist : inlist.join("");
+
+  if (cssinCache.has(param)) {
+    return cssinCache.get(param);
+  }
+
+  if (param.indexOf("{") > 0) {
+    appendCss(param);
+
+    return "";
+  }
+
+  const list = param.split(";");
+  let classname = "";
+
+  list.forEach((str: string) => {
+    str = str.trim();
+    if (str === "") {
+      return;
+    }
+
+    const obj = str.split(":");
+
+    if (obj.length === 1) {
+      if (str[0] === ".") {
+        classname += `${str} `;
+
+        return;
+      }
+      const component = sheets.get(str);
+
+      if (typeof component === "string") {
+        classname += `${cssin(component)} `;
+      }
+
+      return;
+    }
+    // tslint:disable-next-line
+    const name = `c-${str.replace(
+      /[^-0-0a-zA-Z]/g,
+      (reg: string) => `_${reg.charCodeAt(0).toString(16)}_`
+    )}`;
+    let media = obj[obj.length - 4] || "";
+    let hover = obj[obj.length - 3] || "";
+    const sheet = obj[obj.length - 2] || "";
+    if (obj.length === 3 && hover[0] === "@") {
+      media = hover;
+      hover = "";
+    }
+    let value = obj[obj.length - 1] || "";
+    value = value.trim();
+    if (value.indexOf("--") === 0) {
+      value = `var(${value})`;
+    }
+
+    /** 计算并插入css */
+    let css = "";
+    let block = "";
+    const mediaSheet = media[0] === "@" ? sheets.get(media) : "";
+    const cssSheet = sheets.get(sheet);
+
+    block =
+      typeof cssSheet === "function" ? cssSheet(value) : `{${sheet}:${value};}`;
+
+    css = `.${name}${hover ? ":" : ""}${hover} ${block}`;
+
+    if (typeof mediaSheet === "function") {
+      css = mediaSheet(css);
+    }
+
+    /** 插入css, 返回拼接 */
+    appendCss(css);
+    classname += `${name} `;
+  });
+
+  cssinCache.set(param, classname);
+
+  return classname;
+};
