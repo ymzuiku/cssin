eecdcdd3bff45f9242be6bc84df8cc2db1705251
梁柱 <ymblender@gmail.com>
2019-08-11 18:23:48 +0800

    fix: 修复stringfn

--- example-cssin/src/index.tsx
+++ example-cssin/src/index.tsx
@@ -1,15 +1,15 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import { App } from "./App";
-import { coverAttribute } from "./lib";
-import "./lib/commonCSSValues";
-import "./lib/commonSheets";
-import * as serviceWorker from "./serviceWorker";
+import { App } from './App';
+import { coverAttribute } from './lib';
+import './lib/commonCSSValues';
+import './lib/commonSheets';
+import * as serviceWorker from './serviceWorker';
 
-coverAttribute();
+coverAttribute('inlist');
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
