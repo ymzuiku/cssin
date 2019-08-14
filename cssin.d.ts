155aef57c94d46e5abf3d821a9bcc12b6052b0ac
梁柱 <ymblender@gmail.com>
2019-08-14 09:53:44 +0800

    1

--- cssin.d.ts
+++ cssin.d.ts
@@ -3,6 +3,7 @@ interface HTMLElement {
     tempClass?: string;
     useAutoCssin?: boolean;
   };
+  /** cssin 的样式设置，默认相当于设置 setAttribute('class', cssin``), 具体参考 https://github.com/ymzuiku/cssin */
   $cssin(...args: any): this;
 }
 
@@ -11,5 +12,6 @@ interface SVGSVGElement {
     tempClass?: string;
     useAutoCssin?: boolean;
   };
+  /** cssin 的样式设置，默认相当于设置 setAttribute('class', cssin``), 具体参考 https://github.com/ymzuiku/cssin */
   $cssin(...args: any): this;
 }
