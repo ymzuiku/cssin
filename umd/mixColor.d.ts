7516ab039ba04746122eb9d2735026bf5a1d7553
梁柱 <ymblender@gmail.com>
2019-10-18 23:49:42 +0800

    1

--- umd/mixColor.d.ts
+++ umd/mixColor.d.ts
@@ -1,2 +1,8 @@
+export declare function parseColor(str: string): {
+    r: number;
+    g: number;
+    b: number;
+    a: number;
+};
 declare function mix(from: string, to: string, point?: number): string;
 export default mix;
